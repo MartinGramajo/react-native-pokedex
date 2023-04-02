d5a895547a95cfcea65712f4526469a6a2782d45
¨mago¨ <martingramajo08@gmail.com>
2023-04-01 21:58:42 -0300

    stack navigator y config iniciales

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { Navigator } from './src/navigator/Navigator';
+
+export const App = () => {
+  return (
+    <NavigationContainer >
+      <Navigator />
+    </NavigationContainer >
+  );
+};
+
+export default App;
