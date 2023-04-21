f6cad20cc14244a1dc9e090a8219287e0d4cd9c7
¨mago¨ <martingramajo08@gmail.com>
2023-04-20 21:09:19 -0300

    agregamos un infinite scroll

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { ActivityIndicator, FlatList, Image, Text } from 'react-native';
 import { styles } from '../theme/appTheme';
 import { CustomTitle } from '../components/CustomTitle';
 import { usePokemonPaginated } from '../hooks/usePokemonPaginated';
+import { FadeInImage } from '../components/FadeInImage';
 
 
 export const HomeScreen = () => {
 
-  usePokemonPaginated();
+  const { simplePokemonList, loadPokemons } = usePokemonPaginated();
+  console.log("file: HomeScreen.tsx:11 ~ HomeScreen ~ simplePokemonList:", simplePokemonList)
 
   return (
     <>
@@ -15,7 +17,42 @@ export const HomeScreen = () => {
         source={require('../assets/pokebola.png')}
         style={styles.pokebolaBG}
       />
-      <CustomTitle title="Pokedex" />
+      {/* <CustomTitle title="Pokedex" /> */}
+      {/* Con Flatlist vamos a mostrar la data en pantalla  */}
+      <FlatList
+        data={simplePokemonList}
+        keyExtractor={(pokemon) => pokemon.id}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => (
+          // En lugar de utilizar una Image 
+          // utilizamos el useAnimation & FadeInImage
+          // que creamos en clases anteriores.
+          <FadeInImage
+            uri={item.picture}
+            style={{
+              width: 100,
+              height: 100
+            }}
+          />
+          // <Image
+          //   source={{ uri: item.picture }}
+          //   style={{ width: 100, height: 100 }}
+          // />
+        )}
+
+        //Infinite scroll 
+        onEndReached={loadPokemons}
+        // Lo utilizamos para determinar a que nivel de la pantalla
+        // mostraremos el spinner de carga(ActivityIndicator)
+        // Esto equivale al 40% de la pantalla.
+        onEndReachedThreshold={0.4}
+
+        ListFooterComponent={<ActivityIndicator
+          style={{ height: 100 }}
+          size={20}
+          color="grey"
+        />}
+      />
     </>
   );
 };
