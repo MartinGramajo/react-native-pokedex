b893faa7eb5c7b85b26d7239a3214f23100d512a
¨mago¨ <martingramajo08@gmail.com>
2023-04-22 16:09:54 -0300

    agrego efecto en cardPokemon: tomo el color del pokemon para agregar el bg

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import ImageColors from 'react-native-image-colors';
 import { Text, TouchableOpacity, View, StyleSheet, Dimensions, Image } from 'react-native';
 import { SimplePokemon } from '../interfaces/pokemonInterfaces';
 import { FadeInImage } from './FadeInImage';
+
 
 const windowWidth = Dimensions.get('window').width
 
@@ -9,14 +11,69 @@ interface Props {
   pokemon: SimplePokemon;
 }
 
+
+
 export const PokemonCard = ({ pokemon }: Props) => {
+  // Creamos un estado para colocar el bg a la card Pokemon
+  const [bgColor, setBgColor] = useState('grey');
+
+  //Solución al problema del scroll rápido o warning en pantalla.
+  // El problema: al no montarse el componente 
+  // nos tira un warning en la pantalla.
+  // Utilizamos un useRef (no dispara una re renderizacion.
+  const isMounted = useRef(true);
+
+
+  // function que utilizamos en la App de peliculas. 
+  // para tomar el color de la imagen.
+  // const getImageColors = async (uri: string) => {
+  //   const colors = await ImageColors.getColors(uri, {});
+
+  //   let primary;
+
+  //   switch (colors.platform) {
+  //     case 'android':
+  //       primary = colors.dominant;
+
+  //       break;
+
+  //     case 'ios':
+  //       primary = colors.primary;
+
+  //     default:
+  //       break;
+  //   }
+  //   setBgColor(primary || 'grey');
+
+  //   return [primary];
+  // }
+
+
+  // // Usamos UseEffects para disparar el procedimiento de la libreria
+  useEffect(() => {
+    ImageColors.getColors(pokemon.picture, { fallback: 'grey' })
+      .then(colors => {
+        if (!isMounted.current) return;
+
+        (colors.platform === 'android')
+          ? setBgColor(colors.dominant || 'grey')
+          : setBgColor(colors.background || 'grey')
+      })
+    return () => {
+      isMounted.current = false
+    }
+    // getImageColors(pokemon.picture);
+  }, []);
+
+
   return (
     <TouchableOpacity
       activeOpacity={0.9}
     >
       <View style={{
         ...styles.cardContainer,
-        width: windowWidth * 0.4
+        width: windowWidth * 0.4,
+        backgroundColor: bgColor
 
       }}>
         {/* Nombre del pokemon y ID */}
@@ -47,22 +104,22 @@ export const PokemonCard = ({ pokemon }: Props) => {
 const styles = StyleSheet.create({
   cardContainer: {
     marginHorizontal: 10,
-    backgroundColor: 'red',
+    backgroundColor: 'grey',
     height: 120,
     width: 150,
     marginBottom: 25,
     borderRadius: 10,
-    // shadowColor: "#000",
-    // shadowOffset: {
-    //   width: 0,
-    //   height: 2,
-    // },
-    // shadowOpacity: 0.25,
-    // shadowRadius: 3.84,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
   },
   name: {
-    color: 'white',
-    fontSize: 20,
+    color: 'blue',
+    fontSize: 16,
     fontWeight: 'bold',
     top: 20,
     left: 10
